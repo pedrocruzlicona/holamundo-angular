c16cabb34eb4769a4980558a3358ca9596eb8662
Pedro Cruz <pedrocruzmx97@outlook.com>
2019-03-04 21:57:57 -0800

    Cargando todos los archivos del proyecto Hola Mundo

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'component-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Angular';
+  nombre = 'Pedro';
+  apellido = 'Cruz';
+}
---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent{
+
+  year:number;
+  autor:string;
+  constructor(){
+    this.year = new Date().getFullYear();
+    this.autor = "@pedrocruzmx97";
+  }
+  
+
+}
